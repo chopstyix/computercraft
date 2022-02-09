7e60ae7db09e6651fc66b11dc913749f06d12198
chopstyix <76865097+chopstyix@users.noreply.github.com>
2022-02-09 13:35:30 -0800

    Update texas.lua

--- texas.lua
+++ texas.lua
@@ -1,15 +1,17 @@
 --# Monitors
-touchMon = peripheral.wrap("monitor_8")
+mainMon = peripheral.wrap("monitor_8")
 infoMon = peripheral.wrap("monitor_7")
+console = term.redirect(mainMon)
 
 --# Load Images
 local card = paintutils.loadImage("card.nfp")
 local cardBack = paintutils.loadImage("cardback.nfp")
 
---# Configure monitor sizes
+--# Configure monitors
 touchMon.setTextScale(0.5)
 infoMon.setTextScale(0.5)
 
+--# Functions
 function getImageSize(image)
   local height = #image
   local width = 0
@@ -23,3 +25,29 @@ function getImageSize(image)
   return width, height
 end
 
+function drawInfo()
+  local oldMon = term.redirect(infoMon)
+  term.setCursorPos(1,1)
+  term.clearLine()
+  term.setBackgroundColor(colors.green)
+  term.clear()
+  term.write("X: "..x.."  Y: "..y)
+  term.redirect(oldMon)
+end
+
+function drawMain()
+  local oldMon = term.redirect(mainMon)
+  term.setCursorPos(1,1)
+  term.clearLine()
+  term.setBackgroundColor(colors.green)
+  term.clear()
+  paintutils.drawImage(cardBack,5,2) --# Card 1
+  paintutils.drawImage(card,20,2) --# Card 2
+end
+
+--# Begin Main Code
+while true do
+  drawMain()
+  _,side,x,y = os.pullEvent("monitor_touch")
+  drawInfo()
+--# End Main Code
